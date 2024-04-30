85c2234cb520f3cf5f770363770e327518e14778
Maxwell G <maxwell@gtmx.me>
2024-04-30 22:25:32 +0000

    Remove support for Python 3.8

--- noxfile.py
+++ noxfile.py
@@ -48,7 +48,7 @@ def git(session: nox.Session, *args, **kwargs):
 # General
 
 
-@nox.session(python=["3.8", "3.9", "3.10", "3.11", "3.12"])
+@nox.session(python=["3.9", "3.10", "3.11", "3.12"])
 def test(session: nox.Session):
     packages: list[str] = [".[all,tomli,test]"]
     env: dict[str, str] = {}
